76d13d9b628883b731aaac10e0eb9e2080885f6c
Danny Lin <danny@kdrag0n.dev>
2019-08-19 00:36:00 -0700

    Update toolchain to GCC 9.2.0 + binutils 2.32
    Built on an Ubuntu 19.04 system.
    
    Script used: https://gist.github.com/kdrag0n/de18bb3f3a3d697186019e26622fbc06
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- lib/gcc/aarch64-elf/9.1.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "/home/dragon/tcbuild/gcc/build/gcc-9.1.0/configure --target=aarch64-elf --enable-targets=all --prefix=/home/dragon/tcbuild/gcc/9.1.0 --enable-languages=c --without-headers --disable-bootstrap --disable-nls --disable-threads --disable-shared --disable-libmudflap --disable-libssp --disable-libgomp --disable-decimal-float --disable-libquadmath --disable-libatomic --disable-libcc1 --disable-libmpx --enable-checking=release";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { NULL, NULL} };
---
+++ lib/gcc/aarch64-elf/9.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/dragon/tcbuild/gcc/build/gcc-9.2.0/configure --target=aarch64-elf --enable-targets=all --prefix=/home/dragon/tcbuild/gcc/aarch64-elf-9.2.0 --enable-languages=c --without-headers --disable-bootstrap --disable-nls --disable-threads --disable-shared --disable-libmudflap --disable-libssp --disable-libgomp --disable-decimal-float --disable-libquadmath --disable-libatomic --disable-libcc1 --disable-libmpx --enable-checking=release --with-isl --enable-lto --enable-plugin";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
